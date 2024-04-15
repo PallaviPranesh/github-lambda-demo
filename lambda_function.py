313197fe0dc31ccd86cc5932b0f543d7a424f32e
15513 <15513@DESKTOP-9COEV38>
2024-04-14 23:10:26 -0400

    Adding new fileee

---
+++ lambda_function.py
@@ -0,0 +1,10 @@
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    print("This is my first git-lambda function demo py file")
+    print("Here i am adding few lines to print code so that to show this is git-lamda demo")
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
