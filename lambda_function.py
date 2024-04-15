cb615cabd958a0a8bb738da708a5edcac5e43149
PallaviPranesh <78351184+PallaviPranesh@users.noreply.github.com>
2024-04-15 00:10:39 -0400

    Update lambda_function.py
    uu

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    print("This is my first sample git-lambda function demo py file")
+    print("This is my first sampkkle git-lambda function demo py file")
     print("Here i am adding few lines to print code so that to show this is git-lamda demo")
     return {
         'statusCode': 200,
