5eb1c5322c34d6e6693a539505f6db5bd34d2a63
PallaviPranesh <78351184+PallaviPranesh@users.noreply.github.com>
2024-04-16 13:37:08 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,14 @@
+import requests
+
 def lambda_handler(event, context):
-    print("df")
-    print("V1")
+    url = "https://api.quotable.io/random"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        quote_data = response.json()
+        content = quote_data.get("content")
+        author = quote_data.get("author")
+        print(f"Random Quote: '{content}' - {author}")
+        print("v1.1")
+    else:
+        print(f"Failed to retrieve a random quote. Status code: {response.status_code}")
